44d4f0e844d3182046634f2805f6cec8cbc90d34
dleont <dleont@est.ups.edu.ec>
2021-07-21 05:05:03 -0500

    Programando el comportamiento de la moneda (coin)
    1. Hay un mínimo de cinco monedas en el laberinto. Cuando se hace clic en una
    moneda, se reproduce un efecto de sonido en la ubicación de esa moneda.Cuando
    se hace clic en una moneda, esa moneda se elimina de la jerarquía de la escena.
    2. Abra el script de Coin y lea todo el script, incluidos todos los comentarios.
    3. Dedique un tiempo a comprender el comportamiento del objeto prefab CoinPoof
    proporcionado. Puede hacer esto, por ejemplo, ingresando al modo de juego y
    arrastrando un objeto prefab CoinPoof a la escena.
    4. Programe el comportamiento de la moneda completando todos los comentarios
    TODO en el script.

--- A Maze/Assets/UdacityVR/Scripts/Coin.cs
+++ A Maze/Assets/UdacityVR/Scripts/Coin.cs
@@ -2,29 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Coin : MonoBehaviour
+{
 
-	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
-
-
-	void Update () {
-		// OPTIONAL-CHALLENGE: Animate the coin rotating
-		// TIP: You could use a method from the Transform class
-	}
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
+    public GameObject coinPoofPrefab;
+    public AudioClip sonido;
 
 
-	public void OnCoinClicked () {
-		/// Called when the 'Coin' game object is clicked
-		/// - Displays a poof effect (handled by the 'CoinPoof' prefab)
-		/// - Plays an audio clip (handled by the 'CoinPoof' prefab)
-		/// - Removes the coin from the scene
+    void Update()
+    {
+        // OPTIONAL-CHALLENGE: Animate the coin rotating
+        // TIP: You could use a method from the Transform class
+        if (transform.position.y > 0.6f)
+        {
+            transform.Rotate(0, 100 * Time.deltaTime, 0, Space.World);
+        }
+    }
 
-		// Prints to the console when the method is called
-		Debug.Log ("'Coin.OnCoinClicked()' was called");
 
-		// TODO: Display the poof effect and remove the coin from the scene
-		// Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
-		// Use Destroy() to delete the coin after for example 0.5 seconds
-	}
+    public void OnCoinClicked()
+    {
+        /// Called when the 'Coin' game object is clicked
+        /// - Displays a poof effect (handled by the 'CoinPoof' prefab)
+        /// - Plays an audio clip (handled by the 'CoinPoof' prefab)
+        /// - Removes the coin from the scene
+
+        // Prints to the console when the method is called
+        // Debug.Log ("'Coin.OnCoinClicked()' was called");
+
+        // TODO: Display the poof effect and remove the coin from the scene
+        // Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
+        // Use Destroy() to delete the coin after for example 0.5 seconds
+        Object.Instantiate(coinPoofPrefab, this.transform.position, coinPoofPrefab.transform.rotation);
+        Object.Destroy(this.gameObject, 0.5f);
+
+    }
 }
