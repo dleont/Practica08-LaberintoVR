46c26debb6341d670f92c8a14dfe48c084281cfc
dleont <dleont@est.ups.edu.ec>
2021-07-21 05:51:49 -0500

    Programando el comportamiento del SignPost

--- A Maze/Assets/UdacityVR/Scripts/SignPost.cs
+++ A Maze/Assets/UdacityVR/Scripts/SignPost.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 // TODO: Get convenient access the Scene and SceneManager classes
 // Use the 'using' keyword to include the SceneManagement namespace
 
@@ -11,10 +13,12 @@ public class SignPost : MonoBehaviour {
 		/// - Reloads the scene
 
 		// Prints to the console when the method is called
-		Debug.Log ("'SignPost.ResetScene()' was called");
+		// Debug.Log ("'SignPost.ResetScene()' was called");
 
-		// TODO: Reset the scene by getting a reference to the scene and reloading it
-		// Declare a Scene named 'scene', then use SceneManager.GetActiveScene () to get the current scene and assign it to 'scene'
-		// Use SceneManager.LoadScene() and the Scene.name property to reload the scene
+        // TODO: Reset the scene by getting a reference to the scene and reloading it
+        // Declare a Scene named 'scene', then use SceneManager.GetActiveScene () to get the current scene and assign it to 'scene'
+        // Use SceneManager.LoadScene() and the Scene.name property to reload the scene
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
 	}
 }
